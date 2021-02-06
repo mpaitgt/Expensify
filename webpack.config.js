1d6fbb6225cceb1e40347c6047eec8fedadfb157
Matt Pignatore <mattpigs@gmail.com>
2021-02-06 12:58:12 -0500

    Setup production webpack build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,54 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  // webpack config details
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        use: {
-          loader: 'babel-loader'
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    // webpack config details
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          use: {
+            loader: 'babel-loader'
+          },
+          test: /\.js$/,
+          exclude: /node_modules/
         },
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        use: [
-          'style-loader', 
-          'css-loader',
-          'sass-loader'
-        ],
-        test: /\.s?css$/
-      }
-  ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+        {
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          }),
+          test: /\.s?css$/
+        }
+      ]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
